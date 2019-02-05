1987d5115d6567cbd4d1d24628950184338b15fa
Joshua Beverly <beverlyj93@hotmail.com>
2019-02-05 14:58:49 -0600

    Almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,9 @@ var userHelmet = "";
 var userArmor = "";
 var userLegs = "";
 var userWeapon = "";
+var trait = "";
+var name = "";
+var signedIn = false;
 
 $('#sign-up').click(() => {
     if ($('#password').val() === $('#confirm-password').val()) {
@@ -26,105 +29,232 @@ $('#sign-up').click(() => {
         $('#sign-up-status').text("ERROR: Passwords do not match.");
     }
 })
-
+$('.sign-in').click(() => {
+    if (signedIn) {
+        firebase.auth().signOut().then(() => {
+            location.reload();
+        })
+    }
+})
 $('#sign-in').click(() => {
     firebase.auth().signInWithEmailAndPassword($('#sign-in-email').val(), $('#sign-in-password').val()).then(() => {
         $('#sign-in-modal').modal('hide');
-        // Load character
+        retrieveCharacter();
+        signInUser();
     }, (error) => {
         $('#sign-in-status').text(`ERROR: ${error.message}`)
     })
 })
 
 $('#headSelect img').click(function () {
-    if (userHelmet == "") {
-        userHelmet = $(this).attr('data-name');
-        $(this).css('box-shadow', '0px 0px 40px 5px blue');
-        $(this).css('border-radius', '30px');
+    let child = $('#headSelect').children();
+    for (let i = 0; i < child.length; i++) {
+        $(child[i]).attr('selected', false);
+        $(child[i]).removeAttr('style');
     }
+    $(this).attr('selected', 'selected');
+    $(this).css('box-shadow', '0px 0px 40px 5px blue');
+    $(this).css('border-radius', '10px');
 })
 $('#armorSelect img').click(function () {
-    if (userArmor == "") {
-        userArmor = $(this).attr('data-name');
-        $(this).css('box-shadow', '0px 0px 40px 5px blue');
-        $(this).css('border-radius', '30px');
+    let child = $('#armorSelect').children();
+    for (let i = 0; i < child.length; i++) {
+        $(child[i]).attr('selected', false);
+        $(child[i]).removeAttr('style');
     }
+    $(this).attr('selected', 'selected');
+    $(this).css('box-shadow', '0px 0px 40px 5px blue');
+    $(this).css('border-radius', '10px');
 })
 $('#feetSelect img').click(function () {
-    if (userLegs == "") {
-        userLegs = $(this).attr('data-name');
-        $(this).css('box-shadow', '0px 0px 40px 5px blue');
-        $(this).css('border-radius', '30px');
+    let child = $('#feetSelect').children();
+    for (let i = 0; i < child.length; i++) {
+        $(child[i]).attr('selected', false);
+        $(child[i]).removeAttr('style');
     }
+    $(this).attr('selected', 'selected');
+    $(this).css('box-shadow', '0px 0px 40px 5px blue');
+    $(this).css('border-radius', '10px');
 })
 $('#weaponSelect img').click(function () {
-    if (userWeapon == "") {
-        userWeapon = $(this).attr('data-name');
-        $(this).css('box-shadow', '0px 0px 40px 5px blue');
-        $(this).css('border-radius', '10px');
+    let child = $('#weaponSelect').children();
+    for (let i = 0; i < child.length; i++) {
+        $(child[i]).attr('selected', false);
+        $(child[i]).removeAttr('style');
     }
+    $(this).attr('selected', 'selected');
+    $(this).css('box-shadow', '0px 0px 40px 5px blue');
+    $(this).css('border-radius', '10px');
 })
 
 $('#save-character').click(() => {
-    let user = firebase.auth().currentUser;
-    if (user != null) {
-        database.collection('Users').doc().set({
-            Helmet: {
-                HelmetType: userHelmet,
-                HelmetHP: 0
-            },
-            Armor: {
-                ArmorType: userArmor,
-                ArmorHP: 0
-            },
-            Legs: {
-                LegsType: userLegs,
-                LegsHP: 0
-            },
-            Weapon: {
-                WeaponType: userWeapon,
-                WeaponHP: 0
-            },
-            User: {
-                UserID: user.uid,
-                UserHP: 0
-            }
-        })
-    }
-    else $('#creationWarning').html("You are not currently logged in.");
+    saveCharacter();
 })
 
 $('#start-over').click(() => {
-
+    database.collection('Characters').doc($('.character-name').text()).delete().then(() => {
+        location.reload();
+    })
 })
 
-var trait;
-var randomName;
-
-function getTrait() {
-    let word;
-    switch (helmetType) {
-        case "atkHelmet":
-            word = "dangerous";
-        case "defHelmet":
-            word = "strong";
-        default:
-            word = "balanced";
+function signInUser() {
+    if (firebase.auth().currentUser) {
+        let user = firebase.auth().currentUser;
+        $('.btn-sign-up').prop('disabled', true);
+        $('.sign-in').text(`Sign Out (${user.email})`).css('color', 'red');
+        signedIn = true;
     }
-
-    if (word != null) {
-        var traitURL = "https://words.bighugelabs.com/api/2/a85a4a45d7751f10b8e91fe7c7255798/" + word + "/json";
-        $.ajax(traitURL).then(function (response) {
-            let jsonData = JSON.parse(response);
-            var synonym = jsonData.adjective.syn;
-            var trait = synonym[Math.floor(Math.random() * synonym.length)];
-        });
-    };
 }
 
+function loadSelectedParts() {
+    let heads = $('#headSelect').children('img');
+    let armors = $('#armorSelect').children('img');
+    let feets = $('#feetSelect').children('img');
+    let weapons = $('#weaponSelect').children('img');
 
-var nameURL = "https://uinames.com/api/?region=germany&gender=male&minlen=6&maxlen=13&amount=250";
-$.ajax(nameURL).then(function (response) {
-    randomName = response[Math.floor(Math.random(), response.length)].surname;
-    $('#charName').text(trait + "Knight" + randomName);
-});
+    for (let i = 0; i < heads.length; i++) {
+        if ($(heads[i]).attr('selected')) userHelmet = $(heads[i]).attr('data-name');
+    }
+    for (let i = 0; i < armors.length; i++) {
+        if ($(armors[i]).attr('selected')) userArmor = $(armors[i]).attr('data-name');
+    }
+    for (let i = 0; i < feets.length; i++) {
+        if ($(feets[i]).attr('selected')) userLegs = $(feets[i]).attr('data-name');
+    }
+    for (let i = 0; i < weapons.length; i++) {
+        if ($(weapons[i]).attr('selected')) userWeapon = $(weapons[i]).attr('data-name');
+    }
+}
+
+function setName() {
+    $('.character-name').text(`${trait} ${name}`);
+}
+
+function retrieveCharacter() {
+    let user = firebase.auth().currentUser;
+    if (user) {
+        database.collection('Characters').get().then(result => {
+            for (let i = 0; i < result.docs.length; i++) {
+                if (result.docs[i].data().User.UserID == user.uid) {
+                    $('.character-name').text(result.docs[i].id);
+
+                    let heads = $('#headSelect').children();
+                    let armors = $('#armorSelect').children();
+                    let feets = $('#feetSelect').children();
+                    let weapons = $('#weaponSelect').children();
+
+                    for (let j = 0; j < heads.length; j++) {
+                        if ($(heads[j]).attr('data-name') == result.docs[i].data().Helmet.HelmetType) {
+                            $(heads[j]).css('box-shadow', '0px 0px 40px 5px blue');
+                            $(heads[j]).css('border-radius', '10px');
+                            $(heads[j]).attr('selected', 'selected');
+                        }
+                    }
+                    for (let j = 0; j < armors.length; j++) {
+                        if ($(armors[j]).attr('data-name') == result.docs[i].data().Armor.ArmorType) {
+                            $(armors[j]).css('box-shadow', '0px 0px 40px 5px blue');
+                            $(armors[j]).css('border-radius', '10px');
+                            $(armors[j]).attr('selected', 'selected');
+                        }
+                    }
+                    for (let j = 0; j < feets.length; j++) {
+                        if ($(feets[j]).attr('data-name') == result.docs[i].data().Legs.LegsType) {
+                            $(feets[j]).css('box-shadow', '0px 0px 40px 5px blue');
+                            $(feets[j]).css('border-radius', '10px');
+                            $(feets[j]).attr('selected', 'selected');
+                        }
+                    }
+                    for (let j = 0; j < weapons.length; j++) {
+                        if ($(weapons[j]).attr('data-name') == result.docs[i].data().Weapon.WeaponType) {
+                            $(weapons[j]).css('box-shadow', '0px 0px 40px 5px blue');
+                            $(weapons[j]).css('border-radius', '10px');
+                            $(weapons[j]).attr('selected', 'selected');
+                        }
+                    }
+                    break;
+                }
+                return true;
+            }
+        })
+    }
+    return false;
+}
+
+function saveCharacter() {
+    loadSelectedParts();
+
+    let word;
+    switch (userHelmet) {
+        case 'atkHead':
+            word = "dangerous";
+            break;
+        case 'defHead':
+            word = "strong";
+            break;
+        default:
+            word = "tied";
+            break;
+    }
+
+    if (word) {
+        let traitURL = `https://words.bighugelabs.com/api/2/a85a4a45d7751f10b8e91fe7c7255798/${word}/json`;
+        let nameURL = `https://uinames.com/api/?region=germany&gender=male&minlen=6&maxlen=13&amount=250`;
+
+        $.ajax(traitURL).then(response => {
+            let json = JSON.parse(response);
+            let syn = json.adjective.syn;
+            if (trait == "") trait = syn[Math.floor(Math.random() * syn.length)].toUpperCase();
+        })
+
+        $.ajax(nameURL).then(response => {
+            if (name == "") name = response[Math.floor(Math.random(), response.length)].surname.toUpperCase();
+
+            let user = firebase.auth().currentUser;
+            if (user != null) {
+                database.collection('Characters').doc(`${trait.toUpperCase()} ${name.toUpperCase()}`).set({
+                    Helmet: {
+                        HelmetType: userHelmet,
+                        HelmetHP: 0
+                    },
+                    Armor: {
+                        ArmorType: userArmor,
+                        ArmorHP: 0
+                    },
+                    Legs: {
+                        LegsType: userLegs,
+                        LegsHP: 0
+                    },
+                    Weapon: {
+                        WeaponType: userWeapon,
+                        WeaponHP: 0
+                    },
+                    User: {
+                        UserID: user.uid,
+                        BattleID: "",
+                        LastMove: "",
+                        UserHP: 0
+                    }
+                })
+                setName();
+            }
+            else $('#creationWarning').html("You are not currently logged in.");
+        })
+    }
+}
+
+$(document).ready(() => {
+    setTimeout(() => {
+        retrieveCharacter();
+    }, 500);
+    setTimeout(() => {
+        if ($('.character-name').text() != "CHARACTER NAME") {
+            let result = $('.character-name').text();
+            trait = result.split(' ')[0];
+            name = result.split(' ')[1];
+        }
+        if (firebase.auth().currentUser) {
+            signInUser();
+        }
+        else signedIn = false;
+    }, 1000)
+})
